39df1fcfb6291843a706e10fe92d8b5f1af08381
Haelinythii <dwikiiskandar8@gmail.com>
2021-07-26 17:54:05 +0700

    Tutorial & input delay

--- AntiCovid/Assets/Scripts/InputManager.cs
+++ AntiCovid/Assets/Scripts/InputManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    float minTimeToOpenMenu = 0.2f, tapTimer;
+    string hitPlace;
 
     int UILayer;
 
@@ -17,6 +19,14 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0) && IsPointerOverUIElement())
+        {
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            tapTimer -= Time.deltaTime;
+        }
         if (Input.GetMouseButtonDown(0) && !IsPointerOverUIElement()/* !PharmacyManager.instance.pharmacyBuyPanel.activeSelf && !VaksinManager.instance.vaksinPlaceBuyPanel.activeSelf 
             && !HospitalManager.instance.hospitalBuyPanel.activeSelf && !OfficerManager.instance.officerBuyPanel.activeSelf && !Goverment.instance.govermentPanel.activeSelf
             && !GameManager.instance.pausePanel.activeSelf*/)
@@ -25,27 +35,37 @@ public class InputManager : MonoBehaviour
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
             if (hit.collider != null)
             {
-                if (hit.collider.tag == "Hospital")
+                tapTimer = minTimeToOpenMenu;
+                hitPlace = hit.collider.tag;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (tapTimer > 0f)
+            {
+                Debug.Log("WOI");
+                if (hitPlace == "Hospital")
                 {
                     HospitalManager.instance.ShowBuyHospitalPanel(true);
                 }
-                else if(hit.collider.tag == "Pharmacy")
+                else if (hitPlace == "Pharmacy")
                 {
                     PharmacyManager.instance.ShowBuyPharmacyPanel(true);
                 }
-                else if (hit.collider.tag == "Goverment")
+                else if (hitPlace == "Goverment")
                 {
                     //if(!InfoBarInteraction.instance.isCurrentlyShowing)
-                        Goverment.instance.ShowGovermentPanel(true);
+                    Goverment.instance.ShowGovermentPanel(true);
                 }
-                else if (hit.collider.tag == "Officer")
+                else if (hitPlace == "Officer")
                 {
                     OfficerManager.instance.ShowBuyOfficerPanel(true);
                 }
-                else if (hit.collider.tag == "VaksinPlace")
+                else if (hitPlace == "VaksinPlace")
                 {
                     VaksinManager.instance.ShowBuyVaksinPlacePanel(true);
                 }
+                hitPlace = null;
             }
         }
     }
